812ffcd4b96a864ef205e308623c6f907db353cb
晴风 <xuqiang01@bilibili.com>
2022-07-12 10:53:37 +0800

    第二个页面

---
+++ lib/next.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatefulWidget {
+  const NextPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
